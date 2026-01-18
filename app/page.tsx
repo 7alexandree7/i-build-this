99a06b1fc2e96eac03b0d62438f034e9fd962610
alexandre <alexandre020602@gmail.com>
2026-01-18 01:08:16 -0300

    replace folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import FeatureProducts from "@/components/landing-page/feature-products/feature-products";
+import FeatureProducts from "@/components/products/feature-products/feature-products";
 import HeroSection from "@/components/landing-page/hero-section/hero-section";
 
 
